b78d1301cf92299d3608d8bbc21971e88abdfd25
Josh <47374062+dubisis@users.noreply.github.com>
2020-05-04 16:29:51 -0400

    Update beerBot.py

--- beerBot.py
+++ beerBot.py
@@ -2,48 +2,48 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 
 # request
-driver.get('https://shop.rarbrewing.com/')
+driver.get('')
 
 # selecting a product
-cup_wanted = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/main/ul/li[10]/a[1]/img')
+cup_wanted = driver.find_element_by_xpath('')
 cup_wanted.click()
 
 
 # checkout
-checkoutButton = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/main/div[2]/div[2]/form/button')
+checkoutButton = driver.find_element_by_xpath('')
 checkoutButton.click()
 
 # cart view
-cartView = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[1]/div/a')
+cartView = driver.find_element_by_xpath('')
 cartView.click()
 
 # qty button
-qty = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div[2]/main/article/div/div/form/table/tbody/tr[1]/td[5]/div/input')
+qty = driver.find_element_by_xpath('')
 qty.send_keys()
 
 # going to payment center
-go_toPayment = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div[2]/main/article/div/div/div[2]/div/div/a')
+go_toPayment = driver.find_element_by_xpath('')
 go_toPayment.click()
 
 # billing info
-firstName = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div/main/article/div/div/form[3]/div[1]/div[1]/div[1]/div/p[1]/span/input')
+firstName = driver.find_element_by_xpath('')
 firstName.send_keys('James')
-lastName = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div/main/article/div/div/form[3]/div[1]/div[1]/div[1]/div/p[2]/span/input')
+lastName = driver.find_element_by_xpath('')
 lastName.send_keys('Bot')
 
-streetAddress = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div/main/article/div/div/form[3]/div[1]/div[1]/div[1]/div/p[4]/span/input')
-streetAddress.send_keys('123 fuck off lane')
+streetAddress = driver.find_element_by_xpath('')
+streetAddress.send_keys('')
 
 # apt, suite, unit, ect... optional
-unitAddress = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div/main/article/div/div/form[3]/div[1]/div[1]/div[1]/div/p[5]/span/input')
-unitAddress.send_keys('999')
+unitAddress = driver.find_element_by_xpath('')
+unitAddress.send_keys('')
 
-townOrcity = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div/main/article/div/div/form[3]/div[1]/div[1]/div[1]/div/p[6]/span/input')
-townOrcity.send_keys('East hartford')
+townOrcity = driver.find_element_by_xpath('')
+townOrcity.send_keys('')
 
 
-state = driver.find_element_by_xpath('/html/body/span/span/span[1]/input')
-state.send_keys('Connecticut')
+state = driver.find_element_by_xpath('')
+state.send_keys('')
 state.click()
 
 
